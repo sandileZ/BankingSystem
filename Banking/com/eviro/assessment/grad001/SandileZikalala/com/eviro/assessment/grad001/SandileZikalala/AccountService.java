21cdbcc3dfae91e0e25a79e4597580fef8427665
sandileZ <78364274+sandileZ@users.noreply.github.com>
2021-11-25 23:56:33 +0200

    Delete Banking directory

--- Banking/com/eviro/assessment/grad001/SandileZikalala/com/eviro/assessment/grad001/SandileZikalala/AccountService.java
+++
@@ -1,8 +0,0 @@
-package com.eviro.assessment.grad001.SandileZikalala;
-import java.math.BigDecimal;
-
-public interface AccountService 
-{
-    public void withdraw(String accountNum, BigDecimal
-    amountToWithdraw);
-}
